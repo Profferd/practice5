564e038276a4fb95056043f270047cfbb1965566
Profferd <dimagrushko10@gmail.com>
2023-01-16 11:29:02 +0200

    0.9 - added sql init

---
+++ sql/initVal.sql
@@ -0,0 +1,12 @@
+INSERT INTO `authors` (`id`, `name`, `surname`) VALUES (1, 'Stiven', 'King');
+INSERT INTO `authors` (`id`, `name`, `surname`) VALUES (2, 'James', 'Joyce');
+INSERT INTO `authors` (`id`, `name`, `surname`) VALUES (3, 'George', 'Orwell');
+
+INSERT INTO `books` (`id`, `name`, `genre`, `price`, `author_id`) VALUES (1, 'Book123', 'smth', 431, 2);
+INSERT INTO `books` (`id`, `name`, `genre`, `price`, `author_id`) VALUES (2, 'Book name 2', 'fiction', 321, 1);
+INSERT INTO `books` (`id`, `name`, `genre`, `price`, `author_id`) VALUES (3, 'Book4', 'Fiction', 123, 3);
+INSERT INTO `books` (`id`, `name`, `genre`, `price`, `author_id`) VALUES (4, 'Book431', 'History', 42341, 2);
+INSERT INTO `books` (`id`, `name`, `genre`, `price`, `author_id`) VALUES (5, 'Book12', 'Triller', 41, 1);
+INSERT INTO `books` (`id`, `name`, `genre`, `price`, `author_id`) VALUES (6, 'Book512', 'Triller', 4121, 1);
+INSERT INTO `books` (`id`, `name`, `genre`, `price`, `author_id`) VALUES (7, 'Book2342', 'Horror', 921, 1);
+INSERT INTO `books` (`id`, `name`, `genre`, `price`, `author_id`) VALUES (8, 'Book999', 'History', 1, 3);
